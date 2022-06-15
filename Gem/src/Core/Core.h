5fa55acd8122f17e0c8ae559d33c690c88d53e6b
alexander_lindblom <alexander1_lindblom@hotmail.com>
2022-06-15 08:47:34 +0200

    Added logging

---
+++ Gem/src/Core/Core.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifdef GEM_PLATFORM_WINDOWS
+	#ifdef GEM_EXPORT_DLL
+		#define GEM_API __declspec(dllexport)
+	#else
+		#define GEM_API __declspec(dllimport)
+	#endif // GEM_EXPORT_DLL
+#else
+	#error GEM ONLY SUPPORTS WINDOWS AT THE MOMENT!
+#endif GEM_PLATFORM_WINDOWS
+
+#ifdef GEM_DEBUG
+	#ifdef GEM_ENABLE_ASSERTS
+		#define GEML_ASSERT(x, ...) { if(!(x)) {GEM_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+		#define GEM_CORE_ASSERT(x, ...) { if(!(x)) {GEM_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#else
+		#define GEM_ASSERT(x, ...)
+		#define GEM_CORE_ASSERT(x, ...)
+	#endif // GEM_ENABLE_ASSERTS
+
+	#ifdef GEM_ENABLE_VERIFY
+		#define GEM_VERIFY(x, ...) { if(!(x)) {GEM_ERROR("Verification Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+		#define GEM_CORE_VERIFY(x, ...) { if(!(x)) {GEM_CORE_ERROR("Verification Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#else
+		#define GEM_VERIFY(x, ...) x
+		#define GEM_CORE_VERIFY(x, ...) x
+	#endif // GEM_ENABLE_VERIFY
+#endif // GEM_DEBUG
+
+
+#define BIT_SHIFT(x) (1 << x)
---
+++ Gem/src/Core/Log/Log.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Core/Core.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/fmt/ostr.h"
+
+namespace Gem
+{
+
+	class GEM_API Log
+	{
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	};
+
+}
+
+// Macros for core logs
+#define GEM_CORE_TRACE(...)   ::Gem::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define GEM_CORE_INFO(...)    ::Gem::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define GEM_CORE_WARN(...)    ::Gem::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define GEM_CORE_ERROR(...)   ::Gem::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define GEM_CORE_FATAL(...)   ::Gem::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Macros for client logs
+#define GEM_TRACE(...)        ::Gem::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define GEM_INFO(...)         ::Gem::Log::GetClientLogger()->info(__VA_ARGS__)
+#define GEM_WARN(...)         ::Gem::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define GEM_ERROR(...)        ::Gem::Log::GetClientLogger()->error(__VA_ARGS__)
+#define GEM_FATAL(...)        ::Gem::Log::GetClientLogger()->fatal(__VA_ARGS__)
